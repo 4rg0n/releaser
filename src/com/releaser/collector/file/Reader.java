05f4c81dc6727d827a30bc96f5245d0c7e81b9a3
4rg0n <argonthechecker@gmail.com>
2015-06-26 16:57:57 +0200

    added recursive searching for releases functionality

--- src/com/releaser/collector/file/Reader.java
+++ src/com/releaser/collector/file/Reader.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Reads Sub-Directories of a directory
@@ -13,6 +14,10 @@ import java.util.ArrayList;
 public class Reader
 {
     private Path path = null;
+
+    private final ArrayList<Release> releases = new ArrayList<>();
+
+    public static final String RELEASE_IDENTIFIER = ".";
 
     /**
      * Constructor
@@ -47,18 +52,37 @@ public class Reader
             throw new IOException("Folder '" + path + "' does not exist.");
         }
 
-        File[] fileList = folder.listFiles();
-        ArrayList<Release> list = new ArrayList<>();
+        listReleases(path.toFile());
 
-        if (fileList == null) {
-            return list;
+        return releases;
+    }
+
+    /**
+     * Lists all releases in given directory recursively
+     *
+     * @param directory directory to search in
+     */
+    private void listReleases(File directory)
+    {
+        //Get list of all files and folders in directory
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            return;
         }
 
-        for (File file : fileList) {
-            Release release = new Release(file);
-            list.add(release);
-        }
+        //For all files and folders in directory
+        for (File file : files) {
+            //Check if directory
+            if (file.isDirectory()) {
 
-        return list;
+                if (file.getName().contains(RELEASE_IDENTIFIER)) {
+                    releases.add(new Release(file));
+                }
+
+                //Recursively call file list function on the new directory
+                listReleases(file);
+            }
+        }
     }
 }
